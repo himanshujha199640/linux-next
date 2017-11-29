0da11c1d85553bc34b295fcc8f2e4ae700742b37
Himanshu Jha <himanshujha199640@gmail.com>
2017-11-29 16:41:24 +0530

    scsi: fnic: Use eth_broadcast_addr instead of memset
    Use eth_broadcasr_addr Networking API instead of memset to assign the
    broadcast address to the given address array.
    
    0-day tested with no failures.
    
    Signed-off-by: Himanshu Jha <himanshujha199640@gmail.com>

--- drivers/scsi/fnic/fnic_scsi.c
+++ drivers/scsi/fnic/fnic_scsi.c
@@ -276,7 +276,7 @@ int fnic_flogi_reg_handler(struct fnic *fnic, u32 fc_id)
 	}
 
 	if (fnic->ctlr.map_dest) {
-		memset(gw_mac, 0xff, ETH_ALEN);
+		eth_broadcast_addr(gw_mac);
 		format = FCPIO_FLOGI_REG_DEF_DEST;
 	} else {
 		memcpy(gw_mac, fnic->ctlr.dest_addr, ETH_ALEN);
